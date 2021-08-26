633464f2b59dc96a28cdb79c1b6764f2803c7866
10-disha <jayswaldisha5@gmail.com>
2021-08-26 17:12:56 +0530

    operators

---
+++ Chapter 2/01_variable.py
@@ -0,0 +1,12 @@
+a = "disha"
+b=345
+c=45.44
+d=True
+
+#printing variables
+print(a)
+print(b)
+print(d)
+
+#printing type of variables
+print(type(a))
---
+++ Chapter 2/02_operators.py
@@ -0,0 +1,8 @@
+a = 3
+b = 4
+
+#Arithmetic Operators
+print("The value of 3+4 is", 3+4)
+print("The value of 3*4 is", 3*4)
+print("The value of 3-4 is", 3-4)
+print("The value of 3/4 is", 3/4)
