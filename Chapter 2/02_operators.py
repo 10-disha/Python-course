40bf0448b98aca08af731dae628fd39040640a36
10-disha <jayswaldisha5@gmail.com>
2021-08-26 17:20:52 +0530

    operators

--- Chapter 2/02_operators.py
+++ Chapter 2/02_operators.py
@@ -6,3 +6,17 @@ print("The value of 3+4 is", 3+4)
 print("The value of 3*4 is", 3*4)
 print("The value of 3-4 is", 3-4)
 print("The value of 3/4 is", 3/4)
+
+#Assignment Operators
+a=34
+a += 2
+print(a)
+
+# Comparison Operators
+b = (4>7)
+print(b)
+ 
+#Logical operators
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is:", (bool1 and bool2))
