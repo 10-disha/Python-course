eaa208f5f820c0df5fc76cf4c01d958e808850b9
10-disha <jayswaldisha5@gmail.com>
2021-08-31 08:30:41 +0530

    lists and tuples

---
+++ Chapter 4/04_tuple.py
@@ -0,0 +1,13 @@
+#creating a tuple using ()
+t = (1,3,422,33)
+
+# t1 = () #empty tuple
+# t1= (1) #wrong way to declare a Tuple with single element
+t1=(1,) #Tuple with single element
+print(t1)
+
+#printing the elements of a tuple
+# print(t[0])
+
+#cannot update the values of tuple
+# t[0] = 77 #throws an error
