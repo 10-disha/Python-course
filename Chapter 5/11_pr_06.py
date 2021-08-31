653b321943e69d144a88cb2d3d7a201e964ca15f
10-disha <jayswaldisha5@gmail.com>
2021-08-31 17:21:06 +0530

    practicals

---
+++ Chapter 5/11_pr_06.py
@@ -0,0 +1,9 @@
+favLang = { }
+a = input("enter your fav lang disha1\n")
+b = input("enter your fav lang disha2\n")
+c = input("enter your fav lang disha3\n")
+favLang['disha1'] = a
+favLang['disha2'] = b
+favLang['disha3'] = c
+
+print(favLang)
