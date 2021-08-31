5b99fdc1892622078d094f26f36579bff898f4d0
10-disha <jayswaldisha5@gmail.com>
2021-08-31 08:50:20 +0530

    practical list

---
+++ Chapter 4/05_tuple_methods.py
@@ -0,0 +1,4 @@
+t = (1, 2,1, 44, 5)
+
+print(t.count(1))
+print(t.index(5))
---
+++ Chapter 4/07_pr_02.py
@@ -0,0 +1,11 @@
+f1 = int(input("Enter marks Number 1"))
+f2 = int(input("Enter marks Number 2"))
+f3 = int(input("Enter marks Number 3"))
+f4 = int(input("Enter marks Number 4"))
+f5 = int(input("Enter marks Number 5"))
+f6 = int(input("Enter marks Number 6"))
+f7 = int(input("Enter marks Number 7"))
+
+myList = [f1,f2,f3,f4,f5,f6,f7]
+myList.sort()
+print(myList)
