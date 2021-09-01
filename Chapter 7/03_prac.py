d9a9c40c072954a6a67d835177c8327417a56666
10-disha <jayswaldisha5@gmail.com>
2021-09-01 14:09:01 +0530

    lops1

---
+++ Chapter 7/03_prac.py
@@ -0,0 +1,6 @@
+i = 0
+
+while i<5:
+    print("harry" + str(i))
+    i = i +1
+    
---
+++ Chapter 7/04_quick_quiz.py
@@ -0,0 +1,6 @@
+fruits = ['banana', 'watermelon', 'grapes']
+
+i=0
+while i<len(fruits):
+    print(fruits[i])
+    i = i+1
