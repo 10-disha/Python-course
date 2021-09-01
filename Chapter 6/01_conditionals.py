9de660c86a466e20e10b52e3dda34251f1134a0a
10-disha <jayswaldisha5@gmail.com>
2021-09-01 08:12:34 +0530

    conditional

--- Chapter 6/01_conditionals.py
+++ Chapter 6/01_conditionals.py
@@ -1,9 +1,25 @@
 a = 45
-if(a>3):
-    print("the value of a is greater than 3")
-elif(a>7):
-    print("the value of a is greater than 7")
-else:
-    print("the value is not greater than 3 or 7")
+
+#if-elif-else ladder
+# if(a>3):
+#     print("the value of a is greater than 3")
+# elif(a>7):
+#     print("the value of a is greater than 7")
+# else:
+#     print("the value is not greater than 3 or 7")
 
     #only one statement is executed
+
+#2. multiple if statements.    
+
+if(a>3):
+    print("a is greater than 3")
+if(a>7):
+    print("a is greater than 7") 
+else:
+    print("value is not greater than 3 or 7")
+
+print("done")        
+
+
+       
